29123bdeca37b56efd16d96699f4635b7ca0a42b
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-21 12:08:20 -0300

    add arithmetic operator

--- 01 - Facil_Aprendizagem_Java/JavaAprendizadoLivro/src/conteudo/dev2/operador_aritmetico/CB1_ArithmeticOperator.java
+++ 01 - Facil_Aprendizagem_Java/JavaAprendizadoLivro/src/conteudo/dev2/operador_aritmetico/CB1_ArithmeticOperator.java
@@ -3,6 +3,10 @@ package conteudo.dev2.operador_aritmetico;
 public class CB1_ArithmeticOperator {
     // Operador Aritmético: adicionar +, menos -, multiplicar *, divisível /, obter módulo%
 
+    public static int add(int a, int b){
+        return a + b;
+    }
+
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
@@ -12,5 +16,13 @@ public class CB1_ArithmeticOperator {
         System.out.println(a * b);
         System.out.println(a / b);
         System.out.println(c % b); // resto número após a / b
+
+        int result = add(4, 2); // chame o método add
+        System.out.println(result);
+        result = add(5, 3);
+        System.out.println(result);
     }
+
+
+
 }
