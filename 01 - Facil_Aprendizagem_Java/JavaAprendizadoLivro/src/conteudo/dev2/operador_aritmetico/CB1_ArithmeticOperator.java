451296e78f00cc9d71daa8c63ccdc9dfa241b947
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-21 12:14:51 -0300

    add class instanciada class e metodos

--- 01 - Facil_Aprendizagem_Java/JavaAprendizadoLivro/src/conteudo/dev2/operador_aritmetico/CB1_ArithmeticOperator.java
+++ 01 - Facil_Aprendizagem_Java/JavaAprendizadoLivro/src/conteudo/dev2/operador_aritmetico/CB1_ArithmeticOperator.java
@@ -1,10 +1,35 @@
 package conteudo.dev2.operador_aritmetico;
 
+class Caculator{
+    public  int add(int a, int b){
+        return a + b;
+    }
+    public  int sub(int a, int b){
+        return a - b;
+    }
+    public  int multiply(int a, int b){
+        return a * b;
+    }
+    public  int divide(int a, int b){
+        return a / b;
+    }
+}
+
+
 public class CB1_ArithmeticOperator {
     // Operador Aritmético: adicionar +, menos -, multiplicar *, divisível /, obter módulo%
 
     public static int add(int a, int b){
         return a + b;
+    }
+    public static int sub(int a, int b){
+        return a - b;
+    }
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+    public static int divide(int a, int b){
+        return a / b;
     }
 
     public static void main(String[] args) {
@@ -18,11 +43,23 @@ public class CB1_ArithmeticOperator {
         System.out.println(c % b); // resto número após a / b
 
         int result = add(4, 2); // chame o método add
-        System.out.println(result);
-        result = add(5, 3);
-        System.out.println(result);
+        System.out.println(result); //6
+        result = sub(4, 2); // chame o método sub
+        System.out.println(result); //2
+        result = multiply(4, 2); // chame o método multiply
+        System.out.println(result); //8
+        result = divide(4, 2); // chame o método divide
+        System.out.println(result); // 2
+
+
+        Caculator cal = new Caculator();
+        int resultt = cal.add(4, 2); // chame o método add
+        System.out.println(result); //6
+        result = cal.sub(4, 2); // chame o método sub
+        System.out.println(result); //2
+        result = cal.multiply(4, 2); // chame o método multiply
+        System.out.println(result); //8
+        result = cal.divide(4, 2); // chame o método divide
+        System.out.println(result); // 2
     }
-
-
-
 }
